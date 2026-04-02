645889e5ee02f17754d1983444fae29fd3bccbfd
nslobodin <epiforce57@gmail.com>
2026-04-02 13:19:06 +0300

    ios work

--- Source/PrFirebase/Private/iOS/PrFirebaseCrashlyticsModule_iOS.cpp
+++ Source/PrFirebase/Private/iOS/PrFirebaseCrashlyticsModule_iOS.cpp
@@ -14,17 +14,20 @@ void UPrFirebaseCrashlyticsModule_iOS::Crash()
 
 void UPrFirebaseCrashlyticsModule_iOS::SetUserIdentifier(const FString& UserIdentifier)
 {
+	//  todo_ios: ?  (+ also need dispatch async?)
 	[[FIRCrashlytics crashlytics] setUserID:UserIdentifier.GetNSString()];
 }
 
 void UPrFirebaseCrashlyticsModule_iOS::WriteLog(const FString& Log)
 {
-	[[FIRCrashlytics crashlytics] log:Log.GetNSString()];
+	// disabled for now
+	//[[FIRCrashlytics crashlytics] log:Log.GetNSString()];
 }
 
 void UPrFirebaseCrashlyticsModule_iOS::AddAttribute(const FString& Key, const FString& Value)
 {
-	[[FIRCrashlytics crashlytics] setCustomValue:Value.GetNSString() forKey:Key.GetNSString()];
+	// disabled for now
+	//[[FIRCrashlytics crashlytics] setCustomValue:Value.GetNSString() forKey:Key.GetNSString()];
 }
 
 void UPrFirebaseCrashlyticsModule_iOS::Initialize_AnyThread()
